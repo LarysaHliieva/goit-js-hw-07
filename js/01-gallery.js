1682a6272c4cf1e58bdd208e4eac2f1474044f36
LarysaHliieva <lora.glievaya@gmail.com>
2022-11-22 14:45:05 +0200

    created markup and get url

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener("click", onImageClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+        `
+      <div class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${original}"
+          rel="noopener noreferrer nofollow"
+
+        >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-sorce="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+    `).join('');
+}
+
+function onImageClick(e) {
+    e.preventDefault();
+    
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+
+    console.log(e.target.dataset.sorce);
+}
+
